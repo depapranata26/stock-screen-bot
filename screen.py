bbfcac8066121750945c2ff5a937f4a1ee8f9b24
depa <pranatadepa3@gmail.com>
2026-01-15 04:42:45 +0700

    update screen.py

--- screen.py
+++ screen.py
@@ -9,9 +9,7 @@ def screen(df, max_price, min_volume):
     df["low_20d"] = df["low_20d"].astype(float)
 
     mask = (
-        (df["harga"] <= float(max_price)) &
-        (df["volume_5d"] >= float(min_volume)) &
-        (df["harga"] >= df["low_20d"])
+        (df["harga"] <= float(max_price)) &     (df["volume_5d"] >= float(min_volume)) &       (df["harga"] >= df["low_20d"])
     )
 
     return df[mask]
